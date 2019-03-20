950a7fb019bb9e8fac7e8857bead5823e0938a0d
hunterluker <hunterluker1992@gmail.com>
2019-03-19 20:21:53 -0600

    font-awesome cdn

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,6 +14,28 @@ export default class Home extends Component {
         <div className="home-btn">
           <button>Make Contact</button>
         </div>
+
+        <div className="footer">
+          <div className="footer-icons">
+            <a
+              href="https://github.com/hunterluker"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github-square" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/hunterluker/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-linkedin" />
+            </a>
+            <a href="mailto:hunterluker1992@gmail.com">
+              <i className="fas fa-envelope-square" />
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
