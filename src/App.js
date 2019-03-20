9aff7d283ea1386889df7a3dbcfff5c38420d684
hunterluker <hunterluker1992@gmail.com>
2019-03-20 10:32:07 -0600

    add Nav with animations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Nav from './Components/Nav/Nav';
 import routes from './routes';
 import './App.css';
 
@@ -7,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <Router>
+        <Nav />
         <div>{routes}</div>
       </Router>
     );
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import './Nav.css';
+
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      show: false,
+      hidden: true
+    };
+
+    this.toggleShow = this.toggleShow.bind(this);
+  }
+
+  toggleShow() {
+    this.setState({
+      show: !this.state.show,
+      hidden: false
+    });
+  }
+
+  render() {
+    const { show, hidden } = this.state;
+    return (
+      <div className="navbar">
+        <a className="navbar-brand" href="/">
+          <h1>H</h1>
+          <h2>Hunter</h2>
+        </a>
+
+        <div className="bar-menu" onClick={this.toggleShow}>
+          <div className={show ? 'bar1 bar1-change' : 'bar1'} />
+          <div className={show ? 'bar2 hidden-bar' : 'bar2'} />
+          <div className={show ? 'bar3 bar3-change' : 'bar3'} />
+        </div>
+
+        <div
+          className={
+            hidden
+              ? 'navbar-nav hidden'
+              : show
+              ? 'navbar-nav show'
+              : 'navbar-nav no-show'
+          }
+        >
+          <NavLink to="/">
+            <i className="far fa-moon nav-icon" />
+          </NavLink>
+          <NavLink to="/about">
+            <i className="far fa-user nav-icon" />
+          </NavLink>
+          <NavLink to="/skills">
+            <i className="far fa-file-code nav-icon" />
+          </NavLink>
+          <NavLink to="/portfolio">
+            <i className="far fa-eye nav-icon" />
+          </NavLink>
+          <NavLink to="/contact">
+            <i className="far fa-envelope nav-icon" />
+          </NavLink>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Nav);
